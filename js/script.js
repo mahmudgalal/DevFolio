41fb904ca74e1118c706d9b0a3d712986b194df3
mahmudgalal <93224400+mahmudgalal@users.noreply.github.com>
2024-04-20 20:14:47 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const typed = new Typed(".moveletter", {
+    strings: ["Designer" , "Developer" , "Freelancer" , "Photographer"],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 500,
+    smartBackspace: true,
+});
